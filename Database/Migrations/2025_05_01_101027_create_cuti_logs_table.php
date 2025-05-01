7950baab56a97fea4485b3778f0a016962383000
ajil23 <azrilpraya@gmail.com>
2025-05-01 21:21:31 +0800

    fix: hierarchy approval

---
+++ Database/Migrations/2025_05_01_101027_create_cuti_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCutiLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cuti_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('status');
+            $table->unsignedBigInteger('cuti_id');
+            $table->unsignedBigInteger('updated_by');
+            $table->foreign('cuti_id')->references('id')->on('cuti')->onDelete('cascade');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cuti_logs');
+    }
+}
