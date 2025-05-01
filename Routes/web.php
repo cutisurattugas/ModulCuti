bd21e574224c2cfc2d7454e1147793aeae863f9b
ajil23 <azrilpraya@gmail.com>
2025-05-01 12:14:34 +0800

    fix: hierarchy agrrement

--- Routes/web.php
+++ Routes/web.php
@@ -16,14 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('cuti')->group(function () {
     Route::prefix('pengajuan')->group(function () {
         Route::get('/', 'CutiController@index')->name('cuti.index');
-        Route::get('/tambah', 'CutiController@create')->name('cuti.create');
+        Route::get('/create', 'CutiController@create')->name('cuti.create');
         Route::post('/store', 'CutiController@store')->name('cuti.store');
+        Route::get('/show/{id}', 'CutiController@show')->name('cuti.show');
     });
     Route::prefix('jenis')->group(function () {
         Route::get('/', 'JenisCutiController@index')->name('jenis_cuti.index');
-        Route::get('/tambah', 'JenisCutiController@create')->name('jenis_cuti.create');
         Route::post('/store', 'JenisCutiController@store')->name('jenis_cuti.store');
-        Route::get('/edit/{id}', 'JenisCutiController@edit')->name('jenis_cuti.edit');
         Route::put('/update/{id}', 'JenisCutiController@update')->name('jenis_cuti.update');
         Route::delete('/destroy/{id}', 'JenisCutiController@destroy')->name('jenis_cuti.destroy');
     });
